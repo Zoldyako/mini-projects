44e0dd14906eb8865a4befbcc896305edbdf6ca9
José Victor Dias Rodrigues <jose.vdr.pessoal@gmail.com>
2025-08-27 14:51:27 -0300

    Move db connect logic to config/db.js

---
+++ login-signup/server/config/db.js
@@ -0,0 +1,19 @@
+import { Pool } from 'pg'
+import fs from 'fs'
+import 'dotenv/config.js'
+
+const initSql = fs.readFileSync('./mydatabase.sql', 'utf8')
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT,
+})
+
+await pool.on('connect', () => {
+	console.log('Connected to the database!')
+})
+await pool.query(initSql)
+
+export default pool
--- login-signup/server/server.js
+++ login-signup/server/server.js
@@ -1,25 +1,12 @@
 import express from 'express'
 import cors from 'cors'
-import fs from 'fs'
-import { Pool } from 'pg'
 import 'dotenv/config.js'
+import pool from './config/db.js'
 
 const app = express()
 const corsOptions = { origin: ['http://localhost:5173'] }
 const PORT = process.env.SERVER_PORT || 8089
-const pool = new Pool({
-	user: process.env.DB_USER,
-	host: process.env.DB_HOST,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE,
-	port: process.env.DB_PORT,
-})
-const initSql = fs.readFileSync('./mydatabase.sql', 'utf8')
 
-await pool.on('connect', () => {
-	console.log('Connected to the database!')
-})
-await pool.query(initSql)
 
 app.use(cors(corsOptions))
 
